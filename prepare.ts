7024f2c9d04e86a2b8b35b15f5ddf76aee78f62b
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2023-05-13 23:45:27 +0300

    improvement: group async code

--- prepare.ts
+++ prepare.ts
@@ -50,8 +50,11 @@ const README_HEADER = [
 
 async function main() {
   try {
-    const LICENSE = (await fs.readFile(LICENSE_FILE_NAME, { encoding: 'utf8' })).split(/[\r\n]+/).slice(0, 1).join().split(/ +/).slice(0, 1).join()
-    const ADDONS_DIRS = await fs.readdir(ADDONS_DIR)
+    const [LICENSE_CONTENTS, ADDONS_DIRS] = await Promise.all([
+      fs.readFile(LICENSE_FILE_NAME, { encoding: 'utf8' }),
+      fs.readdir(ADDONS_DIR),
+    ])
+    let license = LICENSE_CONTENTS.split(/[\r\n]+/).slice(0, 1).join().replace(/\s+license/i, '')
 
     let readme = README_HEADER
     for (const DIR of ADDONS_DIRS) {
@@ -62,18 +65,19 @@ async function main() {
         author: ADDON_CFG.plugin.author,
         description: ADDON_CFG.plugin.description,
         homepage: `https://github.com/${ADDON_GITHUB_USER}/godot-addons/tree/main/${ADDONS_DIR}/${DIR}#readme`,
-        license: LICENSE,
+        license: license,
         name: `${ADDON_SCOPE}.${DIR}`,
         version: ADDON_CFG.plugin.version,
       }
-      await fs.writeFile(path.join(ADDON_PATH, ADDON_PACKAGE_JSON_FILE_NAME), JSON.stringify(ADDON_PACKAGE_JSON))
-
       readme = [...readme, `- [${ADDON_CFG.plugin.name}](${ADDON_PATH})`]
-      await fs.cp(LICENSE_FILE_NAME, path.join(ADDON_PATH, LICENSE_FILE_NAME))
+      await Promise.all([
+        fs.writeFile(path.join(ADDON_PATH, ADDON_PACKAGE_JSON_FILE_NAME), JSON.stringify(ADDON_PACKAGE_JSON)),
+        fs.cp(LICENSE_FILE_NAME, path.join(ADDON_PATH, LICENSE_FILE_NAME)),
+     ])
     }
     await fs.writeFile(README_FILE_NAME, readme.join('\n'))
   } catch (err) {
-    console.log(err)
+    console.error(err)
   }
 }
 
