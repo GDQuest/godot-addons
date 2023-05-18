78e7586d9f648d0ac7f51e62e9aab8be45bdbba2
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2023-05-18 12:43:46 +0300

    improvement(sparkly_bag): match dirs & files

--- addons/sparkly_bag/sparkly_bag_utils.gd
+++ addons/sparkly_bag/sparkly_bag_utils.gd
@@ -1,12 +1,15 @@
 class_name SparklyBagUtils
 
 
+const SEP := "/"
+
+
 static func fs_find(pattern: String = "*", path: String = "res://") -> Array:
 	var result := []
-	var is_glob = "*" in pattern or "?" in pattern
+	var is_file := not pattern.ends_with(SEP)
 
 	var dir := DirAccess.open(path)
-	if dir.get_open_error() != OK:
+	if DirAccess.get_open_error() != OK:
 		printerr("ERROR: could not open [%s]" % path)
 		return result
 
@@ -18,6 +21,8 @@ static func fs_find(pattern: String = "*", path: String = "res://") -> Array:
 	while path.is_valid_filename():
 		var new_path: String = dir.get_current_dir().path_join(path)
 		if dir.current_is_dir():
+			if path.match(pattern.rstrip(SEP)) and not is_file:
+				result.push_back(new_path)
 			result += fs_find(pattern, new_path)
 		elif path.match(pattern):
 			result.push_back(new_path)
