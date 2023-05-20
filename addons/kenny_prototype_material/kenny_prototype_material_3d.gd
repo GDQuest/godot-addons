14aaefbb0adc97ce52635ab2205be418f8965cd3
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2023-05-20 14:17:35 +0300

    improvement(kenny_prototype_material): white texture for generic albedo color use

--- addons/kenny_prototype_material/kenny_prototype_material_3d.gd
+++ addons/kenny_prototype_material/kenny_prototype_material_3d.gd
@@ -1,24 +1,45 @@
 @tool
 class_name KennyPrototypeMaterial3D extends StandardMaterial3D
 
+const TEXTURES_DIR := "textures"
 
-@export_enum("dark", "green", "light", "orange", "purple", "red") var texture_color := "dark":
+@export var color := Color.WHITE:
 	set(value):
-		texture_color = value
-		set_albedo_texture()
+		color = value
+		albedo_color = color
 
-@export_range(1, 13) var type := 1:
+var textures := []
+
+var type := 0:
 	set(value):
 		type = value
-		set_albedo_texture()
+		for texture in textures.slice(type, type + 1):
+			albedo_texture = load(texture.path)
+			uv1_triplanar = (
+				texture.name.begins_with("plain") or texture.name.begins_with("checkers")
+			)
+			uv1_scale.x = 1 if uv1_triplanar else -1
 
 
 func _init() -> void:
 	super()
-	uv1_triplanar = true
-	set_albedo_texture()
+	var textures_path: String = get_script().resource_path.get_base_dir().path_join(TEXTURES_DIR)
+	textures = (
+		Array(DirAccess.get_files_at(textures_path))
+		. filter(func(fn: String): return not fn.ends_with(".import"))
+		. map(
+			func(fn: String): return {name = fn.get_basename(), path = textures_path.path_join(fn)}
+		)
+	)
+	type = type
 
 
-func set_albedo_texture() -> void:
-	var dir: String = get_script().resource_path.get_base_dir()
-	albedo_texture = load("/".join([dir, "textures", texture_color, "texture_%02d.png" % type]))
+func _get_property_list() -> Array:
+	return [
+		{
+			name = "type",
+			type = TYPE_INT,
+			hint = PROPERTY_HINT_ENUM,
+			hint_string = ",".join(textures.map(func(d: Dictionary): return d.name.capitalize()))
+		}
+	]
